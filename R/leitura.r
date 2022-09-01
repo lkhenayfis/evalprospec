957012e3cc9290e03ba8ec6e3e7b58907609a485
Lucas de Souza Khenayfis <lucask@ons.org.br>
2022-09-01 18:02:20 -0300

    Doc funcoes de leitura e evolui le REE

--- R/leitura.r
+++ R/leitura.r
@@ -1,11 +1,53 @@
 ################################## FUNCOES PARA LEITURA DAS SAIDAS #################################
 
 
-le_arquivoREE <- function(path) {
-    dat <- fread(path)
+# LEITURA DE ARQUIVOS INDIVIDUAIS ------------------------------------------------------------------
+
+#' Leitores De Arquivos
+#' 
+#' Funcoes auxiliares para leitura dos arquivos de saida e formatacao padronizada
+#' 
+#' \code{variavel} e \code{pmo} podem receber valores string diretamente ou NA. Neste caso, a funcao
+#' tenta adivinhar com base no nome do arquivo de qual variavel se trata. 
+#' 
+#' \code{variavel} e advinhada buscando uma sequencia de caracteres seguida de "_", que e o formato
+#' padrao pelo qual os arquivos sao escritos. Caso sua taxonomia seja diferente, valores espurios
+#' podem aparecer.
+#' 
+#' \code{pmo} e adivinhada olhando o nome do diretorio onde \code{arq} se encontra. Tipicamente os 
+#' resultados sao salvos em diretorios nomeados por pmo. Em estruturas de arquivo diferentes, 
+#' valores espurios podem aparecer.
+#' 
+#' @param arq caminho do arquivo a ser lido
+#' @param variavel opcional, a variavel contida no arquivo. Ver Detalhes
+#' @param pmo opcional, o pmo de referencia (string, numerico etc). Ver Detalhes
+#' 
+#' @return data.table padronizado com as colunas
+#' \describe{
+#' \item{pmo}{pmo da simulacao}
+#' \item{REE}{REE do valor simulado}
+#' \item{cenario}{indice do cenario simulado}
+#' \item{data}{data associada ao valor simulado}
+#' \item{variavel}{variavel simulada}
+#' \item{valor}{valor simulado}
+#' }
+
+le_arquivoREE <- function(arq, variavel = NA_character_, pmo = NA_character_) {
+
+    dat   <- fread(arq)
     ncens <- ncol(dat) - 2
     colnames(dat) <- c("data", paste0("cen", seq_len(ncens)), "REE")
     dat <- dat[-1, ]
+
+    dat <- melt(dat, id.vars = c("data", "REE"), variable.name = "cenario", value.name = "valor")
+    dat[, cenario := as.numeric(sub("[[:alpha:]]*", "", cenario))]
+
+    if(is.na(variavel)) variavel <- guess_var(arq)
+    if(is.na(pmo)) pmo <- guess_pmo(arq)
+    dat[, c("variavel", "pmo") := list(variavel, pmo)]
+
+    setcolorder(dat, c("pmo", "REE", "cenario", "data", "variavel", "valor"))
+
     return(dat)
 }
 
