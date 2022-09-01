0cb65b4aa2821ba53fe59563145ec172dd24c6a0
Lucas de Souza Khenayfis <lucask@ons.org.br>
2022-09-01 18:01:38 -0300

    Add funcoes p guess info do dado lido

--- R/leitura.r
+++ R/leitura.r
@@ -16,3 +16,15 @@ le_arquivoSIN <- function(path) {
     dat <- dat[-1, ]
     return(dat)
 }
+
+guess_var <- function(arq) {
+    guess <- sub(".*/", "", arq)
+    guess <- strsplit(guess, "_")[[1]][1]
+    return(guess)
+}
+
+guess_pmo <- function(arq) {
+    guess <- strsplit(arq, "/")[[1]]
+    guess <- tail(guess, 2)[1]
+    return(guess)
+}
