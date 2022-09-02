5f95093db21e27221be2774353da681368dd9b72
Lucas de Souza Khenayfis <lucask@ons.org.br>
2022-09-02 11:55:49 -0300

    Add funcao p selecionar leitor adequado

--- R/utils.r
+++ R/utils.r
@@ -11,3 +11,12 @@ guess_pmo <- function(arq) {
     guess <- tail(guess, 2)[1]
     return(guess)
 }
+
+selec_leitor <- function(tipo) {
+    tipo <- toupper(tipo)
+    switch(tipo,
+        "REE" = le_arquivoREE,
+        "SIN" = le_arquivoSIN,
+        "MERC" = le_arquivoMERC
+    )
+}
