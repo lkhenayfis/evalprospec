139a7f495e754c2c20e13bc5985545f111ecc052
Lucas de Souza Khenayfis <lucask@ons.org.br>
2022-09-16 17:55:30 -0300

    Corrige subsets que estavam por nomes antigos

--- R/metricas_selecao.r
+++ R/metricas_selecao.r
@@ -22,14 +22,14 @@ rank_cenarios <- function(dat, rankfun = rf_earm_sin, quantis = c(.25, .5, .9), 
 
 # FUNCOES DE RANK ----------------------------------------------------------------------------------
 
-rf_eafb_sin <- function(d, ...) {
-    d <- d[variavel == "eafb", tail(valor, 1), by = cenario]
+rf_ena_sin <- function(d, ...) {
+    d <- d[variavel == "ENA", sum(valor), by = cenario]
     setorder(d, V1)
     return(d)
 }
 
 rf_earm_sin <- function(d, ...) {
-    d <- d[variavel == "earm", tail(valor, 1), by = cenario]
+    d <- d[variavel == "EARM", tail(valor, 1), by = cenario]
     setorder(d, V1)
     return(d)
 }
@@ -42,7 +42,7 @@ rf_earm_gter_sin <- function(d, pesos = c(0, 0, 0, 0, 0, 1), ...) {
 }
 
 rf_earm_parana <- function(d, pesos = c(0, 0, 0, 0, 0, 1), ...) {
-    d <- d[(REE == "PARANA") & (variavel == "earm"), weighted.mean(valor, pesos), by = cenario]
+    d <- d[(REE == "PARANA") & (variavel == "EARM"), weighted.mean(valor, pesos), by = cenario]
     setorder(d, V1)
     return(d)
 }
