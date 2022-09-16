955ce3486bee36493a641e0811aca3d49d4d19dd
Lucas de Souza Khenayfis <lucask@ons.org.br>
2022-09-16 17:55:15 -0300

    Melhora estetica dos plots

--- R/visualizacao.r
+++ R/visualizacao.r
@@ -7,10 +7,11 @@ plot.dtsimulREE <- function(x, vars = "EARP", rees = c("PARANA", "SUL", "NORDEST
 
     if(missing("pmos")) pmos <- attr(x, "pmos")
     dplot <- x$simul[(variavel %in% vars) & (REE %in% rees) & (pmo %in% pmos)]
-    dplot[, quadro := paste0(REE, "---", variavel, " --- ", pmo)]
+    dplot[, quadro := paste0(REE, " --- ", variavel, " --- ", pmo)]
 
     gg <- ggplot() +
         geom_line(data = dplot, aes(data, valor, group = cenario), color = "grey80") +
+        labs(x = "Data", y = "Valor") +
         facet_wrap(~ quadro, scales = "free_y", ncol = length(vars) * length(rees), dir = "v") +
         theme_bw()
 
@@ -21,7 +22,8 @@ plot.dtsimulREE <- function(x, vars = "EARP", rees = c("PARANA", "SUL", "NORDEST
         gg <- gg +
             geom_line(data = dplot2, aes(data, valor, group = cenario,
                 linetype = jitter, color = quantil), lwd = 1) +
-            scale_color_viridis_d()
+            scale_color_discrete(name = "Quantil") +
+            scale_linetype_discrete(name = "Offset")
     }
 
     return(gg)
@@ -35,6 +37,7 @@ plot.dtsimulSIN <- function(x, vars = "EARP", pmos, highlight, ...) {
 
     gg <- ggplot() +
         geom_line(data = dplot, aes(data, valor, group = cenario), color = "grey80") +
+        labs(x = "Data", y = "Valor") +
         facet_wrap(~ quadro, scales = "free_y", ncol = length(vars), dir = "v") +
         theme_bw()
 
@@ -45,7 +48,8 @@ plot.dtsimulSIN <- function(x, vars = "EARP", pmos, highlight, ...) {
         gg <- gg +
             geom_line(data = dplot2, aes(data, valor, group = cenario,
                 linetype = jitter, color = quantil), lwd = 1) +
-            scale_color_viridis_d()
+            scale_color_discrete(name = "Quantil") +
+            scale_linetype_discrete(name = "Offset")
     }
 
     return(gg)
@@ -68,9 +72,11 @@ montahighlight <- function(cens) {
     })
     cens <- rbindlist(cens)
 
-    ord <- cens[, as.numeric(sub("j-?", "", unique(jitter)))]
-    ord <- order(abs(ord))
-    cens[, jitter := factor(jitter, levels = unique(jitter)[ord])]
+    lvs <- cens[, as.numeric(sub("j?", "", unique(jitter)))]
+    lbs <- formatC(lvs, format = "d", width = 2, flag = "+")
+    lbs <- sub("\\+0", "0", lbs)
+    ord <- order(abs(lvs))
+    cens[, jitter := factor(jitter, levels = unique(jitter)[ord], labels = lbs[ord])]
     cens
 
     return(cens)
