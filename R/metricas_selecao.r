b506ce41c36340db11dde33c145195f907c0a3e9
Lucas de Souza Khenayfis <lucask@ons.org.br>
2022-09-20 14:23:03 -0300

    Doc rank_cenarios

--- R/metricas_selecao.r
+++ R/metricas_selecao.r
@@ -1,5 +1,27 @@
 ############################### FUNCOES PARA RANKEAMENTO DE CENARIOS ###############################
 
+#' Rankeamento De Cenarios
+#' 
+#' Realiza o rankeamento de N cenarios segundo uma metrica arbitraria \code{rankfun}
+#' 
+#' \code{rank_cenarios} separa o data.table de resultados da simulacao, que pode conter varios pmos, 
+#' em uma lista de data.tables contendo um unico pmo cada. As funcoes de rankeamento \code{rankfun}
+#' devem receber estes data.tables individuais e retorna-los ordenados por criticidade, com o mais
+#' critico na primeira linha e o menos critico na ultima. Por exemplo, para a metrica EARM_SIN_FIM,
+#' a primeira linha corresponde ao cenario com menor EARM e a ultima ao com maior.
+#' 
+#' @param dat objeto do tipo \code{dtsimul} contendo resultados da simulacao dos cenarios a rankear
+#' @param rankfun funcao que recebe data.table de resultados da simulacao para um unico pmo e retorna
+#'     o mesmo data.table ordenado em ordem crescente da metrica. Ver Detalhes
+#' @param quantis opcional, vetor de quantis a selecionar do rankeamento. Se \code{NULL}, retorna o 
+#'     rankeamento completo
+#' @param jitter um inteiro indicando quantos cenarios acima e abaixo daqueles associados aos 
+#'     quantis devem ser retornados, para analise de sensibilidade. Default 0
+#' 
+#' @return se \code{quantis != NULL}, uma lista de dois niveis: primeiro nivel indicando o pmo, 
+#'     segundo nivel os leveis de jitter. Se \code{quantis == NULL}, uma lista de um nivel contendo
+#'     o rankeamento completo para cada pmo
+
 rank_cenarios <- function(dat, rankfun = rf_earm_sin, quantis = c(.25, .5, .9), jitter = 0, ...) {
 
     if(length(jitter) == 1) jitter <- seq(-jitter, jitter)
