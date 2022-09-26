75fa1e42b9b7d2514b5359773026927b43e92ff7
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2022-09-26 12:23:07 -0300

    Add mais um plot pro Hugo

--- main/pmo_202209.r
+++ main/pmo_202209.r
@@ -9,7 +9,7 @@ dir.create(file.path("out", nomebase), recursive = TRUE)
 
 dd_melhor <- le_diretorio(dirbase, "sin", pmo = "pmo_202209")
 
-rank_melhor_full <- rank_cenarios(dd_sin, rf_earm_gter_sin, NA)
+rank_melhor_full <- rank_cenarios(dd_melhor, rf_earm_gter_sin, NA)
 rank_melhor_full <- rank_melhor_full$pmo_202209
 rank_melhor_full[, ecmwf := "melhor"]
 fwrite(rank_melhor_full, file.path("out", nomebase, "rank_melhor.csv"))
@@ -37,6 +37,18 @@ rankk <- list(pmo_202209 = list(j0 = c("10.00%" = 111, "75.00%" = 6)))
 
 gg <- plot(rbind(dd_pior, dd_melhor), highlight = rankk)
 ggsave("out/simul_CFS/pmo_202209/selected.jpeg", gg, width = 9, height = 6)
+
+##############################
+
+rankk2 <- list(pmo_202209 = list(
+    j0 = c("10.00%" = 111, "75.00%" = 6),
+    j1 = c("10.00%" = 89, "75.00%" = 8)
+))
+gg2 <- plot(rbind(dd_pior, dd_melhor), highlight = rankk2) +
+    scale_linetype_discrete(name = "Métrica", labels = c("EAR_GT", "ENA")) +
+    scale_y_continuous(limits = c(0, 100)) +
+    scale_x_date(date_labels = "%b/%Y", date_breaks = "1 month")
+ggsave("out/simul_CFS/pmo_202209/selected_com_ena.jpeg", gg2, width = 9, height = 6)
 
 ##############################
 
